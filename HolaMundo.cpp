f8cfc60a6f1704dd2183a1fb985b6c9d33755f0e
LuiguiNV <luigui_2_10@outlook.es>
2021-09-07 10:09:17 -0500

    Creacion de segundo archivo

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
     cout<<"hola mundo"<<endl;
-
+    cout<<"adios mundo"<<endl;
     return 0;
 }
---
+++ SegundoArchivo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"segundo archivo"<<endl;
+    
+    return 0;
+}
